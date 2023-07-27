e2858e0a7360640d80d55b0c5102eebc1b2cb0b9
Andrea Iosue <li.andrea4@gmail.com>
2023-07-26 22:24:02 -0400

    remove joe todo from joe's calibration

--- api/views/rating.py
+++ api/views/rating.py
@@ -120,8 +120,6 @@ def remove_nonoverlapping_reviewers(data):
     if not con:
         return {}, {r for (r, p, d) in data}
 
-    # TODO @ JOE: pretty sure there is a bug here
-    # con = max(con, key=len)
     con = max(con, key=len, default=[])
 
     new_data = {(r, p, d): y for ((r, p, d), y) in data.items() if r in con}
