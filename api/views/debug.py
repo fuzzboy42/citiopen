09807c85c400bfd0c199b6e6b91629b13f1f0b1b
Andrea Iosue <li.andrea4@gmail.com>
2024-07-16 22:35:55 -0400

    add num years check for out of town rookie

--- api/views/debug.py
+++ api/views/debug.py
@@ -313,7 +313,9 @@ class BulkCreateSignups(APIView):
                 ballkid.is_chairperson = is_chairperson
                 ballkid.phone = phone
                 ballkid.age = age
-                ballkid.is_out_of_town = is_out_of_town_rookie
+                ballkid.is_out_of_town = (
+                    num_years_experience == 0 and is_out_of_town_rookie
+                )
                 ballkid.emergency_name = emergency_name
                 ballkid.emergency_phone = emergency_phone
                 ballkid.image = image
